419fc29aeee2549979346b313417213ce5704e49
Riccardo Venturini <riccardo@fornacestudio.com>
2021-01-09 15:19:23 +0100

    add httpd

--- docker-compose.sh
+++ docker-compose.sh
@@ -5,11 +5,12 @@
 COMPOSE_FILE=docker-compose.base.yml
 
 [ "$ENABLE_MYSQL" = "false" ] || COMPOSE_FILE=${COMPOSE_FILE}:mysql/docker-compose.yml
-[ "$ENABLE_PHP" = "false" ] || COMPOSE_FILE=${COMPOSE_FILE}:php/docker-compose.yml
+[ "$ENABLE_PHP" = "false" ] || COMPOSE_FILE=${COMPOSE_FILE}:php-fpm/docker-compose.yml
 [ "$ENABLE_NGINX" = "false" ] || COMPOSE_FILE=${COMPOSE_FILE}:nginx/docker-compose.yml
+[ "$ENABLE_HTTPD" = "false" ] || COMPOSE_FILE=${COMPOSE_FILE}:httpd/docker-compose.yml
 
 [ -f docker-compose.override.yml ] && COMPOSE_FILE=${COMPOSE_FILE}:docker-compose.override.yml
 
 export COMPOSE_FILE=$COMPOSE_FILE
 
-docker-compose "$*"
+docker-compose $*
