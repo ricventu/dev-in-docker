f1cf499762ea0b3e634a5580e4559e5392cea8c4
Riccardo Venturini <riccardo@fornacestudio.com>
2021-01-09 15:03:07 +0100

    add nginx

--- docker-compose.sh
+++ docker-compose.sh
@@ -6,6 +6,7 @@ COMPOSE_FILE=docker-compose.base.yml
 
 [ "$ENABLE_MYSQL" = "false" ] || COMPOSE_FILE=${COMPOSE_FILE}:mysql/docker-compose.yml
 [ "$ENABLE_PHP" = "false" ] || COMPOSE_FILE=${COMPOSE_FILE}:php/docker-compose.yml
+[ "$ENABLE_NGINX" = "false" ] || COMPOSE_FILE=${COMPOSE_FILE}:nginx/docker-compose.yml
 
 [ -f docker-compose.override.yml ] && COMPOSE_FILE=${COMPOSE_FILE}:docker-compose.override.yml
 
