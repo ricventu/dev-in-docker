99d6a17d56d5c1ba139e2fff51b8da4cb4510e5a
Riccardo Venturini <riccardo@fornacestudio.com>
2021-01-09 15:22:38 +0100

    add redis

--- docker-compose.sh
+++ docker-compose.sh
@@ -8,6 +8,7 @@ COMPOSE_FILE=docker-compose.base.yml
 [ "$ENABLE_PHP" = "false" ] || COMPOSE_FILE=${COMPOSE_FILE}:php-fpm/docker-compose.yml
 [ "$ENABLE_NGINX" = "false" ] || COMPOSE_FILE=${COMPOSE_FILE}:nginx/docker-compose.yml
 [ "$ENABLE_HTTPD" = "false" ] || COMPOSE_FILE=${COMPOSE_FILE}:httpd/docker-compose.yml
+[ "$REDIS_PORT" = "false" ] || COMPOSE_FILE=${COMPOSE_FILE}:redis/docker-compose.yml
 
 [ -f docker-compose.override.yml ] && COMPOSE_FILE=${COMPOSE_FILE}:docker-compose.override.yml
 
