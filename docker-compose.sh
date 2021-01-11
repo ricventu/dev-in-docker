8353df2e1cd239cdd9105b3918fe20f3b452addc
Riccardo Venturini <riccardo@fornacestudio.com>
2021-01-11 09:45:57 +0100

    php multi version

--- docker-compose.sh
+++ docker-compose.sh
@@ -6,10 +6,22 @@ COMPOSE_FILE=docker-compose.base.yml
 
 [ "$ENABLE_MYSQL" = "false" ] || COMPOSE_FILE=${COMPOSE_FILE}:mysql/docker-compose.yml
 [ "$ENABLE_MONGO" = "false" ] || COMPOSE_FILE=${COMPOSE_FILE}:mongo/docker-compose.yml
-[ "$ENABLE_PHP" = "false" ] || COMPOSE_FILE=${COMPOSE_FILE}:php-fpm/docker-compose.yml
 [ "$ENABLE_NGINX" = "false" ] || COMPOSE_FILE=${COMPOSE_FILE}:nginx/docker-compose.yml
 [ "$ENABLE_HTTPD" = "false" ] || COMPOSE_FILE=${COMPOSE_FILE}:httpd/docker-compose.yml
 [ "$REDIS_PORT" = "false" ] || COMPOSE_FILE=${COMPOSE_FILE}:redis/docker-compose.yml
+
+if [ "$ENABLE_PHP73" = "true" ]; then
+  sed "s/__PHP_VERSION__/7.3/g" php/docker-compose-php.template.yml > php/docker-compose-php73.yml
+  COMPOSE_FILE=${COMPOSE_FILE}:php/docker-compose-php73.yml
+  cp php/docker-compose-nginx.template.yml php/docker-compose.nginx.yml
+  echo "      - php7.3" >> php/docker-compose.nginx.yml
+fi
+
+if [ ! "$ENABLE_NGINX" = "false" ]; then
+  COMPOSE_FILE=${COMPOSE_FILE}:nginx/docker-compose.yml
+  [ -e php/docker-compose.nginx.yml ] && COMPOSE_FILE=${COMPOSE_FILE}:php/docker-compose.nginx.yml
+fi
+
 
 [ -f docker-compose.override.yml ] && COMPOSE_FILE=${COMPOSE_FILE}:docker-compose.override.yml
 
