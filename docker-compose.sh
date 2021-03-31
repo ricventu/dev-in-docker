0f373e44965818480f03fa87d89e9217278803ce
Riccardo Venturini <riccardo@fornacestudio.com>
2021-03-31 09:34:22 +0200

    php74

--- docker-compose.sh
+++ docker-compose.sh
@@ -20,6 +20,15 @@ else
   [ -f php/docker-compose.nginx.yml ] && rm php/docker-compose.nginx.yml
 fi
 
+if [ ! "$ENABLE_PHP74" = "false" ]; then
+  sed "s/__PHP_VERSION__/7.4/g" php/docker-compose-php.template.yml > php/docker-compose-php74.yml
+  COMPOSE_FILE=${COMPOSE_FILE}:php/docker-compose-php74.yml
+  cp php/docker-compose-nginx.template.yml php/docker-compose.nginx.yml
+  echo "      - php7.4" >> php/docker-compose.nginx.yml
+else
+  [ -f php/docker-compose.nginx.yml ] && rm php/docker-compose.nginx.yml
+fi
+
 if [ ! "$ENABLE_NGINX" = "false" ]; then
   COMPOSE_FILE=${COMPOSE_FILE}:nginx/docker-compose.yml
   [ -f php/docker-compose.nginx.yml ] && COMPOSE_FILE=${COMPOSE_FILE}:php/docker-compose.nginx.yml
