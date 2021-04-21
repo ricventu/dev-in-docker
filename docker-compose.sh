550cfef4707646e2886667fbfb3e1e253498f717
Riccardo Venturini <riccardo@fornacestudio.com>
2021-04-21 15:39:10 +0200

    GD

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
 
+# defaults
+COMPOSE_PROJECT_NAME=did
+APP_CODE_PATH=./
+EXPORT_CONF=true
+
 [ -e .env ] && . .env
 
 COMPOSE_FILE=docker-compose.base.yml
 
-[ "$ENABLE_MYSQL" != "false" ] && COMPOSE_FILE=${COMPOSE_FILE}:mysql/docker-compose.yml
-[ "$ENABLE_MONGO" != "false" ] && COMPOSE_FILE=${COMPOSE_FILE}:mongo/docker-compose.yml
-[ "$ENABLE_NGINX" != "false" ] && COMPOSE_FILE=${COMPOSE_FILE}:nginx/docker-compose.yml
-[ "$ENABLE_HTTPD" != "false" ] && COMPOSE_FILE=${COMPOSE_FILE}:httpd/docker-compose.yml
-[ "$ENABLE_REDIS" != "false" ] && COMPOSE_FILE=${COMPOSE_FILE}:redis/docker-compose.yml
-[ "$ENABLE_MAILHOG" != "false" ] && COMPOSE_FILE=${COMPOSE_FILE}:mailhog/docker-compose.yml
+[ "$ENABLE_MYSQL" = "true" ] && COMPOSE_FILE=${COMPOSE_FILE}:mysql/docker-compose.yml
+[ "$ENABLE_MONGO" = "true" ] && COMPOSE_FILE=${COMPOSE_FILE}:mongo/docker-compose.yml
+[ "$ENABLE_NGINX" = "true" ] && COMPOSE_FILE=${COMPOSE_FILE}:nginx/docker-compose.yml
+[ "$ENABLE_HTTPD" = "true" ] && COMPOSE_FILE=${COMPOSE_FILE}:httpd/docker-compose.yml
+[ "$ENABLE_REDIS" = "true" ] && COMPOSE_FILE=${COMPOSE_FILE}:redis/docker-compose.yml
+[ "$ENABLE_MAILHOG" = "true" ] && COMPOSE_FILE=${COMPOSE_FILE}:mailhog/docker-compose.yml
 
-if [ ! "$ENABLE_PHP73" = "false" ]; then
+[ -f php/docker-compose.nginx.yml ] && rm php/docker-compose.nginx.yml
+[ -f php/docker-compose-php73.yml ] && rm php/docker-compose-php73.yml
+[ -f php/docker-compose-php74.yml ] && rm php/docker-compose-php74.yml
+
+
+if [ "$ENABLE_PHP73" = "true" ]; then
   sed "s/__PHP_VERSION__/7.3/g" php/docker-compose-php.template.yml > php/docker-compose-php73.yml
   COMPOSE_FILE=${COMPOSE_FILE}:php/docker-compose-php73.yml
   cp php/docker-compose-nginx.template.yml php/docker-compose.nginx.yml
   echo "      - php7.3" >> php/docker-compose.nginx.yml
-else
-  [ -f php/docker-compose.nginx.yml ] && rm php/docker-compose.nginx.yml
 fi
 
 if [ "$ENABLE_PHP74" = "true" ]; then
@@ -25,11 +33,9 @@ if [ "$ENABLE_PHP74" = "true" ]; then
   COMPOSE_FILE=${COMPOSE_FILE}:php/docker-compose-php74.yml
   cp php/docker-compose-nginx.template.yml php/docker-compose.nginx.yml
   echo "      - php7.4" >> php/docker-compose.nginx.yml
-else
-  [ -f php/docker-compose.nginx.yml ] && rm php/docker-compose.nginx.yml
 fi
 
-if [ ! "$ENABLE_NGINX" = "false" ]; then
+if [ "$ENABLE_NGINX" = "true" ]; then
   COMPOSE_FILE=${COMPOSE_FILE}:nginx/docker-compose.yml
   [ -f php/docker-compose.nginx.yml ] && COMPOSE_FILE=${COMPOSE_FILE}:php/docker-compose.nginx.yml
 fi
@@ -38,6 +44,6 @@ fi
 
 export COMPOSE_FILE=$COMPOSE_FILE
 
-[ "$EXPORT_CONF" = "false" ] || docker-compose config > docker-compose.yml
+[ "$EXPORT_CONF" != "false" ] || docker-compose config > docker-compose.yml
 
 docker-compose $*
